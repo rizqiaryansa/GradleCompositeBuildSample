614825b0693af8168ae5ea383dd03b44a62a3bc9
noiz354 <noiz354@gmail.com>
2019-10-15 15:01:41 +0700

    add submodules.

---
+++ groovy/mathutil/mathutil/src/main/groovy/com/sample/math/OperationMath.groovy
@@ -0,0 +1,13 @@
+package com.sample.math
+
+class OperationMath {
+
+    static Double add(Double a, Double b) { return a + b}
+
+    static Double subtracts(Double a, Double b) { return a - b}
+
+    static Double divide(Double a, Double b) { return a / b}
+
+    static Double multiply(Double a, Double b) { return a * b}
+
+}
