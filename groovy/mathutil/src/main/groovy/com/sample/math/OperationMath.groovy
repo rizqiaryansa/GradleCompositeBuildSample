56da5e10f36779bca8f301b12c2711b5cf0fb4c8
Yusuf Hendrawan <yusuf.hendrawan@tokopedia.com>
2019-10-21 18:28:35 +0700

    1. Change grouping for composite enabler and disabler tasks 2. Tidying up script

--- groovy/mathutil/src/main/groovy/com/sample/math/OperationMath.groovy
+++ groovy/mathutil/src/main/groovy/com/sample/math/OperationMath.groovy
@@ -9,5 +9,4 @@ class OperationMath {
     static Double divide(Double a, Double b) { return com.sample.math0.OperationMath.divide(a, b)}
 
     static Double multiply(Double a, Double b) { return a * b}
-
 }
